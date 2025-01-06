058df92bb708f7693f68f49709690ee36f55bc0b
Zuaniratariq15 <zunairat99@gmail.com>
2025-01-06 21:34:09 +0500

    Add files via upload

--- JointAccount.java
+++ JointAccount.java
@@ -1,40 +1,47 @@
-package lab06t1;
-class JointAccount {
-	private int balance = 50000;
-// Synchronized method to handle withdrawals
-	public synchronized void withdraw (String accountHolder, int amount) {
-	System.out.println(accountHolder + " is attempting to withdraw: " + amount);
-	if (amount <= balance) {
-		System.out.println(accountHolder+ successfully withdrew: + amount);
-		balance - amount;
-		}
-	else {
-		System.out.println("Insufficient balance for accountHolder + ". Current balance: " + balance);
-}
-	System.out.println("Remaining balance: " + balance);
-}
-}
-class AccountHolder extends Thread {
-private JointAccount account;
-private String name;
-private int withdrawalAmount;
-public Account Holder (JointAccount account, String name, int withdrawalAmount) {
-this.account account;
-this.name name;
-this.withdrawalAmount withdrawalAmount;
-}
-@Override
-public void run() {
-account.withdraw(name, withdrawalAmount);
-}
-mblic class Main {
-public static void main(String[] args) {
-JointAccount jointAccount new JointAccount();
-// Creating threads for two account holders
-Account Holder userA new AccountHolder(jointAccount, "User A", 45000);
-AccountHolder userB new AccountHolder(jointAccount, "User 8", 20000);
-// Starting threads
-userA.start();
-userB.start();
-
+class Printer {
+private int availablePages 10,
+// Method to add pages to the tray
+public synchronized void addPages (int pages) (
+availablePages + pages;
+System.out.println("Added" pages. pages to the tray. Total pages: availablePages);
+natify();
+// Notify the waiting print thread
+// Method to print pages
+public synchronized void printPages(int pages) (
+System.out.println("Print request received for + pages pages.");
+// Wait until enough pages are available
+while (availablePages pages) (
+System.out.println("Insufficient pages. Waiting for more pages...");
+try (
+wait();
+} catch (InterruptedException e) {
+	wait();
+	} catch (InterruptedException e) (
+	e.printStackTrace();
+	// Proceed with printing
+	availablePages pages;
+	System.out.println("Printed + pages + pages. Remaining pages: + availablePages);
+	}
+	}
+	private Printer printer;
+	private int pagesToPrint;
+	public PrintThread (Printer printer, int pagesToPrint) {
+	this.printer = printer;
+	this.pagesToPrint pagesToPrint;
+	}
+	@Override
+	public void run() {
+	printer.printPages (pagesToPrint);
+	}
+	}
+	public class Main {
+	public static void main(String[] args) (
+	Printer printer new Printer();
+	// Create threads
+	PrintThread print Thread new Print Thread(printer, 15); // Request to print 15 pages
+	AddPages Thread addPages Thread new AddPages Thread(printer); // Adds 10 pages to tray
+	// Start threads
+	printThread.start();
+	addPages Thread.start();
+	}
 }
